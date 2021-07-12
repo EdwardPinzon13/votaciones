4be186c2481cdea55de9796084a9015a305e42e6
Edward Pinzón <edwardpinzon13@gmail.com>
2021-07-12 13:54:53 -0500

    fix/graphHour

--- votaciones/votaciones/settings/local.py
+++ votaciones/votaciones/settings/local.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': get_secret('DB_NAME'),
@@ -15,14 +15,14 @@ ALLOWED_HOSTS = ['*']
         'HOST': 'localhost',
         'PORT': '5432',
     }
-}'''
-
+}
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS =  [BASE_DIR / 'static']
