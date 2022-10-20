1ad6b80b84efed6eb0f657861bcdf4c38f797c37
EdwardPinzon13 <empinzon13@gmail.com>
2022-10-20 06:35:51 -0500

    add static root

--- votaciones/votaciones/settings/local.py
+++ votaciones/votaciones/settings/local.py
@@ -26,10 +26,12 @@ DATABASES = {
 }
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS =  (os.path.join(BASE_DIR,'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
