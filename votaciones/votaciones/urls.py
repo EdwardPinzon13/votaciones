dbd3465ccb8332390665f8e84f5b67744031bcf1
EdwardPinzon13 <empinzon13@gmail.com>
2022-10-20 06:48:48 -0500

    add static root

--- votaciones/votaciones/urls.py
+++ votaciones/votaciones/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('applications.votantes.urls')),
     path('', include('applications.users.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
